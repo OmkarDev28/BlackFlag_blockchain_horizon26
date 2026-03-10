4fbbd5ec8fae3f884ead15a34a74976057d684d5
Piyush Vishwakarma <pvvishwa12345@gmail.com>
2026-03-10 16:40:07 +0530

    Update INR.sol

--- blockchain/contracts/INR.sol
+++ blockchain/contracts/INR.sol
@@ -7,8 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract INR is ERC20, Ownable {
     constructor() ERC20("Digital INR", "INR") Ownable(msg.sender) {}
 
-    // In a real scenario, only the Central Bank or a regulated custodian can mint.
-    // For this prototype, we'll allow the owner (Clearing Corp) to mint.
+
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
